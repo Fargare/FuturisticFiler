932af971b88b7b7fe1a0cc564ca5b2e7e6780543
Fargare <gleaming7moonlight29@gmail.com>
2017-08-01 19:32:38 +0900

    フォルダ移動できるようになった。

--- filer.js
+++ filer.js
@@ -5,112 +5,26 @@
  const ASSETS = {
   image: {
     bg: "FFiler_BG.png",
+    folder:"Folder.png",
     // bgm:"FF.png",
   },
 };
 const SCREEN_WIDTH = window.parent.screen.availWidth;
 const SCREEN_HEIGHT= window.parent.screen.availHeight;
 console.log(SCREEN_WIDTH,SCREEN_HEIGHT);
-let path = "C:/Users/kaoru/Pictures/";
-let current_files = [];
-// グローバルに展開
-let fs = require('fs');
-fs.readdir(path, function(err,files){
-  if (err) throw err;
-  // for (key in files){
-  //   current_files[key] = files[key];
-  // }
-  Array.prototype.push.apply(current_files,files);
-  // console.log(files);
-});
-// console.log("current_files");
-console.log(current_files);
 
-// ASSETS.image.push(current_files);
-// ASSETS.image.concat(current_files);
-// console.log(ASSETS.image);
+let image_size = 400;
+
+let current_path = ['C:/Users','kaoru','Pictures'];
+
+let path = '';
+let current_files = [];
+let current_num = 0;
+let fs = require('fs');
+
+readDirectory();
 
 phina.globalize();
-
-phina.define('FileImage',{
-  superClass: 'phina.display.DisplayElement',
-  init: function(image, width, height) {
-    this.superInit();
-
-    this.srcRect = phina.geom.Rect();
-    this.setImage(image, width, height);
-  },
-
-  draw: function(canvas) {
-    var image = this.image.domElement;
-
-    // canvas.context.drawImage(image,
-    //   0, 0, image.width, image.height,
-    //   -this.width*this.origin.x, -this.height*this.origin.y, this.width, this.height
-    //   );
-
-    var srcRect = this.srcRect;
-    canvas.context.drawImage(image,
-      srcRect.x, srcRect.y, srcRect.width, srcRect.height,
-      -this._width*this.originX, -this._height*this.originY, this._width, this._height
-      );
-  },
-
-  setImage: function(image, width, height) {
-    if (typeof image === 'string') {
-      image = phina.asset.AssetManager.get('image', image);
-    }
-    this._image = image;
-    this._image.src = path+current_files[0];
-    // console.log(this._image,image);
-    this.width = this._image.domElement.width;
-    this.height = this._image.domElement.height;
-
-    this.frameIndex = 0;
-
-    if (width) { this.width = width; }
-    if (height) { this.height = height; }
-
-    return this;
-  },
-
-  setFrameIndex: function(index, width, height) {
-    var tw  = width || this._width;      // tw
-    var th  = height || this._height;    // th
-    var row = ~~(this.image.domElement.width / tw);
-    var col = ~~(this.image.domElement.height / th);
-    var maxIndex = row*col;
-    index = index%maxIndex;
-
-    var x = index%row;
-    var y = ~~(index/row);
-    this.srcRect.x = x*tw;
-    this.srcRect.y = y*th;
-    this.srcRect.width  = tw;
-    this.srcRect.height = th;
-
-    this._frameIndex = index;
-
-    return this;
-  },
-
-  _accessor: {
-    image: {
-      get: function() {return this._image;},
-      set: function(v) {
-        this.setImage(v);
-        return this;
-      }
-    },
-    frameIndex: {
-      get: function() {return this._frameIndex;},
-      set: function(idx) {
-        this.setFrameIndex(idx);
-        return this;
-      }
-    },
-  },
-});
 
 phina.define('MainScene', {
   superClass: 'DisplayScene',
@@ -124,68 +38,81 @@ phina.define('MainScene', {
     this.bg = Sprite("bg",SCREEN_WIDTH,SCREEN_HEIGHT).addChildTo(this);
     this.bg.origin.set(0,0);
 
-    this.circle = CircleShape({
-      x:this.gridX.center(),
-      y:this.gridY.center(),
-      fill:'transparent',
-      stroke:'yellow',
-      strokeWidth:10,
-      radius:0,
-    }).addChildTo(this);
-
-    this.circle2 = CircleShape({
-      x:this.gridX.center(5),
-      y:this.gridY.center(),
-      fill:'transparent',
-      stroke:'yellow',
-      radius:0,
-    }).addChildTo(this);
-
-    this.circle3 = CircleShape({
-      x:this.gridX.center(-5),
-      y:this.gridY.center(),
-      fill:'transparent',
-      stroke:'yellow',
-      radius:0,
-    }).addChildTo(this);
+    // this.circle = CircleShape({
+    //   x:this.gridX.center(),
+    //   y:this.gridY.center(),
+    //   fill:'transparent',
+    //   stroke:'white',
+    //   strokeWidth:10,
+    //   radius:0,
+    // }).addChildTo(this);
+    //
+    // this.circle2 = CircleShape({
+    //   x:this.gridX.center(5),
+    //   y:this.gridY.center(),
+    //   fill:'transparent',
+    //   stroke:'white',
+    //   radius:0,
+    // }).addChildTo(this);
+    //
+    // this.circle3 = CircleShape({
+    //   x:this.gridX.center(-5),
+    //   y:this.gridY.center(),
+    //   fill:'transparent',
+    //   stroke:'white',
+    //   radius:0,
+    // }).addChildTo(this);
 
     this.label = Label({
-      text:current_files,
+      text:path,
       x:this.gridX.center(),
       y:this.gridY.center(3),
-      fill:'yellow',
+      fill:'white',
+      fontFamily:'Century Gothic',
+      fontSize:50,
     }).addChildTo(this);
 
     this.img = new Image();
-    this.img.src = path+current_files[0];
-    console.log(this.img.src);
-    this.space = Sprite('bg',100,100).addChildTo(this);
+
+    // this.img.src = path+current_files[current_num];
+    // console.log(this.img.src);
+    this.space = Sprite('bg',image_size,image_size).addChildTo(this);
+    this.folder = Label({
+      text :"",
+      x:this.space.width/2,
+      y:this.space.height/2,
+      fill:'white',
+      fontFamily:'Century Gothic',
+      fontSize:50,
+    }).addChildTo(this.space);
+    this.filename = Label({
+      text :"test",
+      x:this.space.width/2,
+      y:this.space.height + 20,
+      fill:'white',
+      fontFamily:'Century Gothic',
+      fontSize:30,
+    }).addChildTo(this.space);
+
+    readPicture(this);
+
     const self = this;
     this.space.draw = function(canvas){
-      canvas.context.drawImage(self.img,0,0);
+      let width = image_size;
+      let height = image_size;
+      if (self.img.width>self.img.height){
+        height = image_size * (self.img.height/self.img.width);
+      }
+      else{
+        width = image_size * (self.img.width/self.img.height);
+      }
+      canvas.context.drawImage(self.img,image_size/2-width/2,image_size/2-height/2,width,height);
     }
-    // console.log(current_files[0]);
-    // // console.log((path+current_files[0]));
-    // let image1 = FileImage('bg',200,200).addChildTo(this);
-    // console.log(image1);
-
-    // canvas
-    // this._dirtyDraw = true;
-    //
-    // this.rectangle = BackGround({
-    //   height:SCREEN_HEIGHT,
-    //   width:SCREEN_WIDTH,
-    //   x:SCREEN_WIDTH/2,
-    //   y:SCREEN_HEIGHT/2,
-    //   fill:'transparent',
-    // }).addChildTo(this);
-
-    this.count = 0;
-    // this.circle.alpha = 0;
+    this.count = 2;
 
   },
-  update:function(canvas){
-
+  update:function(app){
+    const self = this;
     if(this.count ==0){
       this.circle.tweener.clear()
       .to({
@@ -206,11 +133,106 @@ phina.define('MainScene', {
       },200,"swing")
       .setLoop(false);
       this.count = 2;
-      this.img.src = path + current_files[1];
     }
-  }
+    const key = app.keyboard;
+    if (!this.space.tweener.playing){//キーボード入力はじめ
+      if (key.getKey('left')){
+        this.space.tweener.clear()
+        .to({
+          alpha:0,
+        },100)
+        .call(function(){
+          current_num = (current_num+current_files.length-1)%current_files.length;
+          readPicture(self);
+        })
+        .to({
+          alpha:1,
+        },100)
+        .setLoop(false);
+      };
+      if (key.getKey('right')){
+        this.space.tweener.clear()
+        .to({
+          alpha:0,
+        },100)
+        .call(function(){
+          current_num = (current_num+1)%current_files.length;
+          readPicture(self);
+        })
+        .to({
+          alpha:1,
+        },100)
+        .setLoop(false);
+      };
+      if (key.getKey('up')){
+        current_path.pop();
+        readDirectory();
+        this.space.tweener.clear()
+        .to({
+          alpha:0,
+        },100)
+        .call(function(){
+          current_num = 0;
+          readPicture(self);
+          self.label.text = path;
+        })
+        .to({
+          alpha:1,
+        },100)
+        .setLoop(false);
+      };
+      if (key.getKey('down')){
+        // console.log(current_files[current_num].match(/\./));
+        if(!/\./.test(current_files[current_num])){
+          // console.log("Folder!!!");
+          current_path.push(current_files[current_num]);
+          readDirectory();
+          this.space.tweener.clear()
+          .to({
+            alpha:0,
+          },100)
+          .call(function(){
+            current_num = 0;
+            readPicture(self);
+            self.label.text = path;
+          })
+          .to({
+            alpha:1,
+          },100)
+          .setLoop(false);
+        }
+      };
+    };//キーボード入力終わり
+  },
 
 });
+function readPicture(self){
+  if(!/jpg|JPG|PNG|png/.test(current_files[current_num])){
+    self.img.src = "folder.png";
+    self.folder.text = current_files[current_num];
+    self.filename.text = '';
+    // current_num = (current_num+1)%current_files.length;
+  }
+  else{
+    self.img.src = path+current_files[current_num];
+    self.folder.text = "";
+    self.filename.text = current_files[current_num];
+  }
+};
+function readDirectory(){
+  path = '';
+  current_path.forEach(function(value){
+    path += value + '/';
+  });
+  current_files = [];
+  // console.log(path);
+  fs.readdir(path, function(err,files){
+    if (err) throw err;
+    Array.prototype.push.apply(current_files,files);
+    console.log(current_files);
+    console.log(files);
+  });
+}
 
 /*
  * メイン処理
